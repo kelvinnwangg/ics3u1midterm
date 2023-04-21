404e1969573f58891cbfaf696ce6a40e5b7f8751
kelvin <kelvin.wang24@ycdsbk12.ca>
2023-04-21 00:02:33 -0400

    Updated almost all scenes...
    Added box around text to make it more readable in all scenes, added simple animation Ln71-78, Finished basics for Scenes 5-8

--- midtermproject.java
+++ midtermproject.java
@@ -7,50 +7,52 @@ public class midtermproject{
 	public static void main(String[] args){
 		Console con = new Console("Eggcellent Adventure", 1280, 720);
 		
-		Font fntCousine = con.loadFont("Cousine-Regular.ttf", 20);
-		con.setDrawFont(fntCousine);
-		
 		//Scene 1
 		String strChoice1;
 		
 		// Loads and prints --.jpg, Initializes strChoice2 as the input from the user
 		BufferedImage imgScene1 = con.loadImage("Scene1.jpg");
 		con.drawImage(imgScene1, 0, 0);
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 120);
 		con.setTextColor(Color.BLACK);
 		con.println("Lady Goose laid many eggs, including one golden egg. It gets stolen by a sneaky wizard by the name of...");
 		con.sleep(2500);
 		con.println("... Harry Pottuh!");
 		con.sleep(1500);
-		con.println("Do you, as a hungry villager, decide to 'HELP' Lady Goose find her egg or 'COOK' her up for dinner?");
-		strChoice1 = con.readLine();
-		
-		//If the user enters "cook", clears current console, runs scene2, runs scene22
-		if(strChoice1.equalsIgnoreCase("Cook")){
-			con.clear();
-			scene2(con);
-			scene2D(con);
-		// If user enters "help", clears current console, runs scene3
-		}else if(strChoice1.equalsIgnoreCase("Help")){
-			con.clear();
-			scene3(con);
+			while(true){
+				con.println("Do you, as a hungry villager, decide to 'HELP' Lady Goose find her egg or 'COOK' her up for dinner?");
+				strChoice1 = con.readLine();
+				//If the user enters "cook", clears current console, runs scene2, runs scene22
+				if(strChoice1.equalsIgnoreCase("Cook")){
+					con.clear();
+					scene2(con);
+					scene2D(con);
+				// If user enters "help", clears current console, runs scene3
+				}else if(strChoice1.equalsIgnoreCase("Help")){
+					con.clear();
+					scene3(con);
+				}
+			}
 		}
-	}
 		//Scene 2
 	public static void scene2(Console con){
-		char chrLetter;
+		char chrKey1;
 		
 		//Loads and prints --.jpg
 		BufferedImage imgScene2 = con.loadImage("Scene2.jpg");
+		con.drawImage(imgScene2, 0, 0);
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 120);
 		con.setTextColor(Color.BLACK);
 		con.println("You are starving and decide to catch and cook Lady Goose up for dinner...");
-		con.sleep(1500);
+		con.sleep(500);
 		con.println("Click 'c' to continue...");
-		con.drawImage(imgScene2, 0, 0);
 		
 		// Grabs the current keyboard input, if input is "c", runs Line 24
 		while(true){
-			chrLetter = con.currentChar();
-			if(chrLetter == 'c'){
+			chrKey1 = con.currentChar();
+			if(chrKey1 == 'c'){
 				return;
 			}
 		}
@@ -60,45 +62,202 @@ public class midtermproject{
 		BufferedImage imgScene22 = con.loadImage("Scene22.jpg");
 		con.drawImage(imgScene22, 0, 0);
 		con.clear();
-		con.sleep(5000);
+		con.println("MmMmMmMMMmMmm... tastes goo-");
+		con.sleep(2000);
+		con.println("BLEH");
+		
+		int intCount;
+		int intCount2;
+		for(intCount2 = 0; intCount2 <3; intCount2++){
+		for(intCount = 400; intCount >0; intCount = intCount - 8){
+			con.drawImage(imgScene22, 0, 0);
+			con.setDrawColor(Color.RED);
+			con.drawString("WASTED", 700, intCount+200);
+			con.repaint();
+			// 30 fps = 1000ms/30 = 33ms about
+			con.sleep(33);
+		}
+	}
+			
+		con.sleep(3000);
 		con.closeConsole();
 	}
 		// Scene 3
-	public static void scene3(Console con){
+	private static void scene3(Console con){
 		String strChoice2;
-		
+		strChoice2 = "";
 		// Loads and prints --.jpg, Initializes strChoice2 as the input from the user
 		BufferedImage imgScene3= con.loadImage("Scene3.jpg");
 		con.drawImage(imgScene3, 0, 0);
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 120);
+		
 		con.println("You decide to dedicate your time to help Lady Goose find her precious golden egg.");
 		con.sleep(1500);
 		con.println("You walk for a bit and come across some strange boot prints...");
 		con.sleep(1500);
-		con.println("Do you choose to 'ignore' the footprint or further 'look into' it");
-		strChoice2 = con.readLine();
 		
-		if(strChoice2.equalsIgnoreCase("Ignore")){
-			con.clear();
-			scene4(con);
-		}else if(strChoice2.equalsIgnoreCase("look into")){
-			con.clear();
-			scene5(con);
-		}
+		while(!strChoice2.equalsIgnoreCase("Ignore") || !strChoice2.equalsIgnoreCase("look into")){
+			con.println("Do you choose to 'ignore' the footprint or further 'look into' it");
+			strChoice2 = con.readLine();
+				if(strChoice2.equalsIgnoreCase("Ignore")){
+					con.clear();
+					scene4(con);
+				}else if(strChoice2.equalsIgnoreCase("look into")){
+					con.clear();
+					scene5(con);
+				}
+			}
 	}
 	// Scene 4
 	public static void scene4(Console con){
 		// Loads and prints --.jpg, 
 		BufferedImage imgScene4= con.loadImage("Scene4.jpg");
 		con.drawImage(imgScene4, 0, 0);
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 120);
+		
 		con.println("Oh no... You chose to ignore the OBVIOUS clue.");
 		con.sleep(1500);
 		con.println("You continue on your useless \" search\"  and eventually starve to death");
-		con.sleep(3000);
+		con.sleep(5000);
 		con.closeConsole();
 	}
+	// Scene 5
 	public static void scene5(Console con){
+		
+		int intChoice3;
+		
+		con.clear();
 		BufferedImage imgScene5= con.loadImage("Scene5.jpg");
 		con.drawImage(imgScene5, 0, 0);
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 120);
+		
 		con.println("You decide to look into the footprint");
+		con.sleep(1500);
+		con.println("You see that it leads to 2 distinct paths...");
+		con.sleep(1500);
+		con.println("The LEFT path seems to lead to a swamp");
+		con.sleep(1500);
+		con.println("While the RIGHT path seems to lead to a strange, old shed... maybe a wizard shed??");
+		con.sleep(1500);
+		con.println("Which path do you go down? 1 - Left OR 2 - Right (IF YOU DONT ENTER A VALID NUMBER YOU DIE)");
+		intChoice3 = con.readInt();
+		
+		if(intChoice3 == 1){
+			con.clear();
+			scene6(con);
+		}else if(intChoice3 == 2){
+			con.clear();
+			scene8(con);
+		}else{
+			con.println("What did i say...");
+			con.println("AIYAAAAAA~");
+			con.sleep(1000);
+			con.closeConsole();
+		}
+	}
+	// Scene 6
+	public static void scene6(Console con){
+		char chrLetter;
+		
+		BufferedImage imgScene6= con.loadImage("Scene6.jpg");
+		con.drawImage(imgScene6, 0, 0);
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 120);
+		
+		con.println("You picked the path on the left...");
+		con.sleep(1500);
+		con.println("Couldnt you see it CLEARLY led to a swamp??...");
+		con.sleep(1500);
+		con.println("You decide to walk beside the swamp, hopefully finding more clues...");
+		con.sleep(500);
+		con.println("Click 'c' to continue");
+		
+		while(true){
+			chrLetter = con.currentChar();
+		if(chrLetter == 'c'){
+			con.clear();
+			scene7(con);
+		}
+	}
+	}
+	// Scene 7
+	public static void scene7(Console con){
+		BufferedImage imgScene7= con.loadImage("Scene7.jpg");
+		con.drawImage(imgScene7, 0, 0);
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 120);
+		
+		con.println("On your walk beside the swamp, you see something poking out of the water...");
+		con.sleep(1500);
+		con.println("You decide to go check it out. Maybe its a clue?");
+		con.sleep(1500);
+		con.println("WRONG");
+		con.sleep(1500);
+		con.println("The 'thing' was an aligator and it jumps out of the water and eats you");
+		con.println("You get chomp chomped");
+		con.sleep(5000);
+		con.closeConsole();
+	}
+	// Scene8
+	public static void scene8(Console con){
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 720);
+		
+		char chrKey2;
+		con.println("You pick the path on the right...");
+		con.println("It DOES lead to a wizard shed, a seemingly abandoned one");
+		con.println("You find Harry Pottuh and DEMAND the egg back, but he won't give in");
+		con.println("He offers you a deal, testing your knowledge");
+		con.println("He asks you a few questions and says if you get them right, he'll give the egg back");
+		con.println("Click 'c' to continue");
+		
+		while(true){
+			chrKey2 = con.currentChar();
+		if(chrKey2 == 'c'){
+			con.clear();
+			scene8a(con);
+		}
+	}
+}
+	// Scene 8a
+	public static void scene8a(Console con){
+		BufferedImage imgScene7= con.loadImage("Scene8.jpg");
+		con.drawImage(imgScene7, 0, 0);
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 120);
+		int intRandom;
+
+		// Choices for Level 1 difficulty questions
+		int intChoice5;
+		int intChoice6;
+		double dblChoice7;
+		double dblChoice8;
+		// Choices for Level 2 difficulty questions
+		int intChoice9;
+		int intChoice10;
+		double dblChoice11;
+		double dblChoice12;
+		
+		intRandom = (int) Math.random()*2+1;
+		if(intRandom == 1){
+			con.println("What is 2+9?");
+			con.println("What is 2*7?");
+			con.println("Is 'CAR' a palindrome?");
+			con.println("Is grass usually green?");
+		}else if(intRandom == 2){
+			con.println("What is 'pi/3' (radians) in degrees?");
+			con.println("What is 2(3-1)+12");
+			con.println("Are black and white a 'colour' or 'shade' ?");
+			con.println("Is Winnie The Pooh 'yellow' or 'blue' ?");
+		}
+	}
+	public static void scene8death(Console con){
+		con.setDrawColor(new Color(237, 235, 168));
+		con.fillRect(0, 0, 1280, 720);
+		
+		
 	}
 }
